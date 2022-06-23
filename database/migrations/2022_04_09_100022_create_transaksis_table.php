d5df21d5dade6306f66472d6464b330456eaf698
Alfinata Yusuf Sitaba <alfinata.sitaba@gmail.com>
2022-06-23 12:01:31 +0700

    Co-authored-by: Dindin Inas Candra Wiguna <Dindin14@users.noreply.github.com> Co-authored-by: JEHUA KUSUMA DEWA <jehuakusumadewa@365.telkomuniversity.ac.id> Co-authored-by: Rafif Fausta Kusuma Syam <pippuu@users.noreply.github.com>

---
+++ database/migrations/2022_04_09_100022_create_transaksis_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id('ID_Transaksi');
+            $table->date('tanggal_sewa');
+            $table->date('tanggal_kembali');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksis');
+    }
+};
